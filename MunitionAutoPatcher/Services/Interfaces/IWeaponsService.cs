187f4c3e3dde4c16095b5c32ea0e45e011ae9017
shkondo <kondo.shun.io@tut.jp>
2025-10-24 01:53:56 +0900

    WIP: local changes before pulling remote

--- MunitionAutoPatcher/Services/Interfaces/IWeaponsService.cs
+++ MunitionAutoPatcher/Services/Interfaces/IWeaponsService.cs
@@ -10,4 +10,5 @@ public interface IWeaponsService
     Task<List<WeaponData>> ExtractWeaponsAsync(IProgress<string>? progress = null);
     Task<WeaponData?> GetWeaponAsync(FormKey formKey);
     List<WeaponData> GetAllWeapons();
+    List<AmmoData> GetAllAmmo();
 }
